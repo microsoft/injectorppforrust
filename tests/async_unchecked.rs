429bf56ed0aa36500c4a53f6f469bd255a7f0b9b
Jingyu Ma (from Dev Box) <jingm@microsoft.com>
2025-06-22 16:11:34 -0700

    Update the tests

--- tests/async_unchecked.rs
+++ tests/async_unchecked.rs
@@ -27,7 +27,7 @@ impl HttpClientTest {
 }
 
 #[tokio::test]
-async fn test_simple_async_unchecked() {
+async fn test_simple_async_func_unchecked_should_success() {
     let mut injector = InjectorPP::new();
 
     unsafe {
@@ -41,9 +41,25 @@ async fn test_simple_async_unchecked() {
     let x = simple_async_func_u32_add_one(1).await;
     assert_eq!(x, 123);
 
-    // other async function unaffected
-    let x2 = simple_async_func_u32_add_two(1).await;
-    assert_eq!(x2, 3);
+    // simple_async_func_u32_add_two should not be affected
+    let x = simple_async_func_u32_add_two(1).await;
+    assert_eq!(x, 3);
+
+    unsafe {
+        injector
+            .when_called_async_unchecked(injectorpp::async_func_unchecked!(
+                simple_async_func_u32_add_two(u32::default())
+            ))
+            .will_return_async_unchecked(injectorpp::async_return_unchecked!(678, u32));
+    }
+
+    // Now because it's faked the return value should be changed
+    let x = simple_async_func_u32_add_two(1).await;
+    assert_eq!(x, 678);
+
+    // simple_async_func_bool should not be affected
+    let y = simple_async_func_bool(true).await;
+    assert_eq!(y, true);
 
     unsafe {
         injector
@@ -53,19 +69,22 @@ async fn test_simple_async_unchecked() {
             .will_return_async_unchecked(injectorpp::async_return_unchecked!(false, bool));
     }
 
+    // Now because it's faked the return value should be false
     let y = simple_async_func_bool(true).await;
     assert_eq!(y, false);
 }
 
 #[tokio::test]
-async fn test_complex_struct_async_unchecked() {
-    // Test GET method
+async fn test_complex_struct_async_func_unchecked_without_param_should_success() {
     {
+        // This is a temporary instance that is needed for async function fake.
+        // Parameter does not matter.
         let temp_client = HttpClientTest {
             url: String::default(),
         };
 
         let mut injector = InjectorPP::new();
+
         unsafe {
             injector
                 .when_called_async_unchecked(injectorpp::async_func_unchecked!(temp_client.get()))
@@ -75,6 +94,7 @@ async fn test_complex_struct_async_unchecked() {
                 ));
         }
 
+        // Now the real client will be used and its behavior should be faked
         let real_client = HttpClientTest {
             url: "https://test.com".to_string(),
         };
@@ -83,25 +103,30 @@ async fn test_complex_struct_async_unchecked() {
         assert_eq!(result, "Fake GET response".to_string());
     }
 
-    // After injector dropped, original behavior restored
     let real_client = HttpClientTest {
         url: "https://test.com".to_string(),
     };
 
+    // The original function should be called as the injector is out of scope
     let result = real_client.get().await;
     assert_eq!(result, "GET https://test.com".to_string());
+}
 
-    // Test POST method
+#[tokio::test]
+async fn test_complex_struct_async_func_unchecked_with_param_should_success() {
     {
+        // This is a temporary instance that is needed for async function fake.
+        // Parameter does not matter.
         let temp_client = HttpClientTest {
             url: String::default(),
         };
 
         let mut injector = InjectorPP::new();
+
         unsafe {
             injector
                 .when_called_async_unchecked(injectorpp::async_func_unchecked!(
-                    temp_client.post("payload")
+                    temp_client.post("test payload")
                 ))
                 .will_return_async_unchecked(injectorpp::async_return_unchecked!(
                     "Fake POST response".to_string(),
@@ -109,19 +134,20 @@ async fn test_complex_struct_async_unchecked() {
                 ));
         }
 
+        // Now the real client will be used and its behavior should be faked
         let real_client = HttpClientTest {
             url: "https://test.com".to_string(),
         };
 
-        let result = real_client.post("payload").await;
+        let result = real_client.post("test payload").await;
         assert_eq!(result, "Fake POST response".to_string());
     }
 
-    // After injector dropped, original behavior restored
     let real_client = HttpClientTest {
         url: "https://test.com".to_string(),
     };
 
-    let result = real_client.post("payload").await;
-    assert_eq!(result, "POST payload to https://test.com".to_string());
+    // The original function should be called as the injector is out of scope
+    let result = real_client.post("test payload").await;
+    assert_eq!(result, "POST test payload to https://test.com".to_string());
 }
